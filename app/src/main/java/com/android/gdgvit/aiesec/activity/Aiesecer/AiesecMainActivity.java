29352ed97371ba30c85c64c256aac473bf5ee4d8
TheAlgo <dhirajkumar.jain2016@vitstudent.ac.in>
2017-11-27 01:29:38 +0530

    A

--- app/src/main/java/com/android/gdgvit/aiesec/activity/Aiesecer/AiesecMainActivity.java
+++ app/src/main/java/com/android/gdgvit/aiesec/activity/Aiesecer/AiesecMainActivity.java
@@ -87,7 +87,7 @@ public class AiesecMainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        System.out.println("Hello");
+        System.out.println("Hi");
         if (id == R.id.aiescer_profile2) {
             //toolbar.setVisibility(View.GONE);
             AiescerProfileFragment df = new AiescerProfileFragment();
